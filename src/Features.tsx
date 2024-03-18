7fe90f2ae3831750b82993b8d314bd910df9995f
Hothaifa <hothaifa.jaber@altatweer.com>
2024-03-18 09:18:50 +0300

    updated

--- src/Features.tsx
+++ src/Features.tsx
@@ -4,14 +4,14 @@ export default function Features() {
   return (
     <>
       <div>
-        <h1 className="text-4xl font-bold text-default-700 text-center mb-5">
+        <h1 className="text-4xl font-bold text-default-700 text-center mb-8">
           المميزات الاساسية في الخدمة
         </h1>
         {/* Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5 gap-y-10">
           {[...Array(10 - 1 + 1)].map((_, index) => (
             <div
-              className="card bg-white shadow-md p-5 rounded-3xl rounded-tl-none"
+              className="card bg-white shadow-sm p-5 rounded-[2rem] rounded-tl-none"
               key={`card-${index}`}
             >
               <div className="w-12 h-12 bg-gradient-to-br from-default-500 to to-default-600 rounded-2xl flex items-center justify-center text-white mb-3">
