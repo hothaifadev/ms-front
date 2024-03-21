911f5d155b0dbc272abbf35ed6929b3bef0fcf0d
Hothaifa <hothaifa.jaber@altatweer.com>
2024-03-21 12:14:50 +0300

    update

--- src/components/fragment.tsx
+++ src/components/fragment.tsx
@@ -12,10 +12,10 @@ export type FragmentType = {
 export default function Fragment(props: FragmentType) {
   useEffect(() => {
     Aos.init();
-  });
+  }, []);
   return (
     <div className="relative text-center px-3 py-20  mx-auto mb-5">
-      <div className="absolute left-10 bottom-0" aos-data="fade-up">
+      <div className="absolute left-10 bottom-0">
         <div className="blur-3xl opacity-70 w-40 h-40 rounded-full bg-indigo-500/50"></div>
       </div>
       <div className="absolute right-0 top-10">
@@ -27,12 +27,14 @@ export default function Fragment(props: FragmentType) {
         }`}
       >
         <div className="prose lg:max-w-xl lg:mx-auto">
-          <h1 className="text-default-800 mb-10" aos-data="fade-up">
+          <h1 className="text-default-800 mb-10" data-aos-duration="200">
             {props.title}
           </h1>
           <p className="mb-7 md:mb-0"> {props.description}</p>
         </div>
         <img
+          data-aos="fade-down"
+          data-aos-duration="400"
           src={props.image}
           alt="صورة توضيحية للتصميم"
           className="md:max-w-md lg:max-w-2xl w-auto mx-auto drop-shadow-lg"
